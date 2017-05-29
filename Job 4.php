a657500cf0831dafe7b7551aae4a5f7857f99496
SITIFADHILAH <dedhexs@yahoo.com>
2017-05-29 13:53:07 +0700

    Add files via upload

---
+++ Job 4.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<?php
+$kampusku="STEKOM";
+$elamat1="Jl. Majapahit 304 Semarang";
+$elamat2="Jl. Majapahit 605 Semarang";
+echo"<a href=\"link2b.php?kampusku=$kampusku&
+alamatl=$alamatl&alamat2\">Kampusku </a>";
+?>
+</body>
+</html>
